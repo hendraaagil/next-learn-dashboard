2991d7499412d8e8f46b3cff044820adf5d922c0
hendraaagil <hendraaagil@gmail.com>
2023-11-03 19:03:09 +0700

    feat: add fonts and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { plusJakartaSans } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${plusJakartaSans.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Lusitana, Plus_Jakarta_Sans } from 'next/font/google';
+
+export const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
